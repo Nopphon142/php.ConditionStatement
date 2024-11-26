38a29254d4c005b8b90b56aadb7a356d39949138
Nopphon142 <66040233142@udru.ac.th>
2024-11-26 11:41:09 +0700

    update

---
+++ test_get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ทดสอบส่งค่าโดยใช้ Method GET</title>
+</head>
+<body>
+    <h2>ทดสอบรับค่าจาก method GET (ผ่าน URL)</h2>
+    <?php
+        echo "การศึกษา:".$GET['subject'].".$_GET['web'];
+    ?>
+</body>
+</html>
